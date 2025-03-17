e66f781d1de0aa04a6613c0b699c517d6ea57ea3
Melquijava <melqueribeiroamorim@gmail.com>
2025-03-17 00:35:58 -0300

    Melhorando Códigos

--- commands/entrar.py
+++ commands/entrar.py
@@ -4,22 +4,27 @@ from discord.ext import commands
 class Entrar(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print("Cog Entrar inicializado!")  # Log para depuração
+        print("Cog Entrar inicializado!")
 
     @commands.command()
     async def entrar(self, ctx):
-        print("Comando entrar foi chamado!")  # Log para depuração
-        if ctx.author.voice:
-            channel = ctx.author.voice.channel
-            await channel.connect()
-            await ctx.send(f'Entrei no canal de voz: {channel}')
-        else:
-            await ctx.send('Você precisa estar em um canal de voz!')
+        print("Comando entrar foi chamado!")
+        try:
+            if ctx.author.voice:
+                channel = ctx.author.voice.channel
+                if ctx.voice_client is not None:
+                    await ctx.voice_client.move_to(channel)
+                    await ctx.send(f'Movido para o canal de voz: {channel}')
+                else:
+                    await channel.connect()
+                    await ctx.send(f'Entrei no canal de voz: {channel}')
+            else:
+                await ctx.send('Você precisa estar em um canal de voz!')
+        except Exception as e:
+            print(f"Erro ao entrar no canal de voz: {e}")
+            await ctx.send(f"Ocorreu um erro ao entrar no canal de voz. Erro: {e}")
 
 async def setup(bot):
-    print("Carregando Cog Entrar...")  # Log para depuração
+    print("Carregando Cog Entrar...")
     await bot.add_cog(Entrar(bot))
-    print("Cog Entrar carregado!")  # Log para depuração
-
-# faça um print para saber que esse arquivo foi executado
-print('entrar.py foi executado!')
+    print("Cog Entrar carregado!")
--- commands/sair.py
+++ commands/sair.py
@@ -4,9 +4,11 @@ from discord.ext import commands
 class Sair(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        print("Cog Sair inicializado!")
 
     @commands.command()
     async def sair(self, ctx):
+        print("Comando sair foi chamado!")
         if ctx.voice_client:
             await ctx.voice_client.disconnect()
             await ctx.send('Saí do canal de voz.')
@@ -14,4 +16,6 @@ class Sair(commands.Cog):
             await ctx.send('Não estou em um canal de voz.')
 
 async def setup(bot):
+    print("Carregando Cog Sair...")
     await bot.add_cog(Sair(bot))
+    print("Cog Sair carregado!")
